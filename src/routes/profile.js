be8fc8a06c90e77c04df3877199c7650c682f054
Naman-1234 <namankalrabhiwani54@gmail.com>
2021-05-12 18:04:04 +0530

    Added route for Updaing User Profile

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -3,12 +3,36 @@ const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const auth = require("../middlewares/auth");
+const brcypt = require("bcryptjs");
 router.get("/", auth, async (req, res) => {
   try {
     const user = req.user;
     res.status(200).send(user);
   } catch (err) {
     res.status(404).send("No One found");
+  }
+});
+router.patch("/:id", auth, async (req, res) => {
+  try {
+    const { name, email, gender, phoneNo } = req.body;
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        name: name,
+        email: email,
+        gender: gender,
+        phoneNo: phoneNo,
+      },
+      {
+        new: true,
+        useValidators: true,
+      }
+    );
+    if (!user) res.status(401).send("Enter correct credentials please!!");
+    res.status(201).send();
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
   }
 });
 module.exports = router;
